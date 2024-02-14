80dc847cb1bb56d61655b41dacb87da827b0ca23
Viral Bhoi <141637546+viralbhoi@users.noreply.github.com>
2024-02-14 20:39:36 +0530

    Add files via upload

---
+++ 10-01-2024/hellobvm.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello BVM!");
+    return 0;
+}
---
+++ 10-01-2024/myinfo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Name : Viral Bhoi\n");
+    printf("ID : 23CP029\n");
+    printf("Address:\n");
+    printf("A1 Navrachana society;\n");
+    printf("Vallabh Vidhyanagar;\n");
+    return 0;
+}
